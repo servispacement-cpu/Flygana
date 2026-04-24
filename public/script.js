0210a222d9009dfeeb21f7a5d82699986d3055de
Aaron Maller <servispacement@gmail.com>
2026-04-24 12:43:34 +0200

    rf

--- public/script.js
+++ public/script.js
@@ -100,11 +100,13 @@ const url = `https://flygana.onrender.com/place/${vol.Nvol}`;
 
         if (!response.ok) throw new Error(`Erreur HTTP : ${response.status}`);
         const data = await response.json();
-        console.log("data pour places :" + data);
+        await console.log("data pour places :" + data);
         if (data.vplaces < data.places){
             volssuite(vol);
         } else {
             alert("Désolé, il n'y a plus de places dans l'avion");
+            alert(data.vplaces)
+            alert(data.places)
         }
     } catch (error) {
         console.error('Erreur :', error);
